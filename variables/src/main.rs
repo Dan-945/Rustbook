aa4901356d539288fd2e439d73c7552a37f42aa7
Dan Helgeland <dan.helgeland@gmail.com>
2025-01-06 19:46:48 +0100

    added ownership

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,4 +1,51 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
-    let asd = "asd";
+    /*
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    
+    
+    
+    
+    let element = a[index];
+
+    println!("The value of the element at index {index} is: {element}");*/
+
+    
+    print_labeled_measurement(5, 'h');
+
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("The value of y is: {y}");
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {result}");
+}
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value} {unit_label}");
 }
